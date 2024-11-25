e54f8472edd6b717ff058d69cc07faeb1d131e0f
Antarese <quanglh.kts@gmail.com>
2024-11-25 16:34:06 +1100

    trying to clean up the code and make it more efficient

--- Godot/Scripts/Game_Manager/Test_01/Environment.gd
+++ Godot/Scripts/Game_Manager/Test_01/Environment.gd
@@ -1,4 +1,4 @@
-extends TileMap
+"""extends TileMap
 
 # Constants
 const MAX_LAYERS = 5
@@ -80,7 +80,80 @@ func remove_highlight():
 		if previous_tile_data.has("z"): #Ensure the data is valid
 			#Remove the highlight by setting the tile ID to -1 at the highlight layer
 			set_cell(previous_tile_data["z"] + 1, hovered_tile, -1)
-		hovered_tile = null  #Reset hovered_tile to indicate no tile is currently highlighted
+		hovered_tile = null  #Reset hovered_tile to indicate no tile is currently highlighted"""
 
+extends TileMap
 
+#Constants
+const MAX_LAYERS = 5
+const MAX_Z_INDEX = 5
+const HIGHLIGHT_TILE_ID = 5 #The id of the sprite tile used as highlight
+
+#Variables
+var tile_data = {} #Stores tile data (z_index and coordinates)
+var hovered_tile = null #Current hovered tile coordinates (Vector2i)
+
+func _ready():
+	prepare_tile_data()
+
+func prepare_tile_data():
+	#Initialize and clean storage
+	compute_tile_data()
+	remove_occluded_tile()
+
+func compute_tile_data():
+	tile_data.clear()
+	for z in range(MAX_Z_INDEX):
+		for cell in get_used_cells(z):
+			var coords = Vector2i(cell.x, cell.y)
+			#Store only the highest z_index at each coordinates
+			if not tile_data.has(coords) or tile_data[coords].z < z:
+				tile_data[coords] = {"z":z, "coords":coords}
+
+func remove_occluded_tile():
+	#Remove tiles occluded by higher tiles in the tile column
+	var to_remove = []
+	for coords in tile_data.keys():
+		var z = tile_data[coords].z
+		var occluding_coords = coords + Vector2i(-1,-1)
+		if tile_data.has(occluding_coords) and tile_data[occluding_coords].z > z:
+			to_remove.append(coords)
+	for coords in to_remove:
+		tile_data.erase(coords)
+
+func get_tile_coords_under_mouse() -> Vector2i:
+	#Convert global mouse position to Tilemap coordinates
+	return local_to_map(get_global_mouse_position())
+
+func get_tile_data(coords: Vector2i) -> Dictionary:
+	#Retrieve tile data for the given coordinates
+	var current_tile_data = tile_data.get(coords)
+	if current_tile_data:
+		return current_tile_data
+	else:
+		return {}
+
+func apply_highlight(z:int, coords: Vector2i):
+	#Add highlight at mouse coordinates but at a higher z index
+	set_cell(z+1, coords, HIGHLIGHT_TILE_ID)
+
+func clear_highlight():
+	#Remove the highligh when the mouse no longer hovers over it
+	if hovered_tile:
+		var prev_tile_info = tile_data[hovered_tile].z
+		if prev_tile_info:
+			set_cell(prev_tile_info + 1, hovered_tile, -1)
+		hovered_tile = null
+
+func _process(delta):
+	#Highlight tile under mouse
+	var mouse_coords = get_tile_coords_under_mouse()
+	var tile_info = get_tile_data(mouse_coords)
+	if tile_info:
+		if mouse_coords != hovered_tile:
+			clear_highlight()
+			apply_highlight(tile_info["z"], mouse_coords)
+			hovered_tile = mouse_coords
+	else:
+		clear_highlight()
 
