def2c2166cb16d53f1d567149993968d1d33b30e
Antarese <quanglh.kts@gmail.com>
2025-07-22 01:49:54 +1000

    Added tile highlighting system
    Added function for tracking mouse positions.
    Added function for tile highlighting.
    Added function for remove highlighting.

--- Godot/Scripts/Game_Manager/Test_01/Environment.gd
+++ Godot/Scripts/Game_Manager/Test_01/Environment.gd
@@ -15,29 +15,48 @@ const UNSELECTED_LAYER_OPACITY: float = 0.25
 const OCCLUDED_SOURCE_ID_STEP: int = 100
 const OCCLUDED_ATLAS_COORDS_STEP: Vector2i = Vector2i(0, 0)
 const OCCLUDED_ALTERNATIVE_TILE_STEP: int = 0
+const HIGHLIGHT_SOURCE_ID_STEP: int = 200
+const HIGHLIGHT_ATLAS_COORDS_STEP: Vector2i = Vector2i(0, 0)
+const HIGHLIGHT_ALTERNATIVE_TILE_STEP: int = 0
 const INVALID_SOURCE_ID = -1
+const UNUSED_VECTOR2I = Vector2i(-999, -999)
+const UNUSED_VECTOR3I = Vector3i(-999, -999, -999)
 
 
 # Dictionary[Vector3i, Dictionary] - Maps 3D coordinates to tile data
 var debug_mode := true
 var all_tiles_dict: Dictionary = {} 
 var current_z: int = MAX_Z
-var current_max_z: int = MIN_Z
+var current_layer: TileMapLayer
+var level_max_z: int = MIN_Z
 var child_count: int = 0
+var last_highlighted_coords: Vector2i = UNUSED_VECTOR2I
+var currently_highlighted_tile: Vector3i = UNUSED_VECTOR3I
 
 
 func _ready() -> void:
 	collect_all_tiles()
 	collect_occluded_tiles()
-	set_current_max_z()
+	set_level_max_z()
+	set_current_layer()
 	apply_occluded_tiles_effect()
 
 
-func _input(_event):
+func _input(event: InputEvent) -> void:
 	var old_z = current_z
 	move_current_layer()
 	if old_z != current_z:
 		apply_unselected_layer_effect()
+	if event is InputEventMouseMotion:
+		handle_mouse_movement()
+
+
+func handle_mouse_movement() -> void:
+	var current_coords = get_mouse_coords()
+	
+	if current_coords != last_highlighted_coords:
+		apply_highlighted_tile_effect()
+		last_highlighted_coords = current_coords
 
 
 func collect_all_tiles() -> void:
@@ -56,7 +75,9 @@ func collect_all_tiles() -> void:
 			all_tiles_dict[coords] = {
 				"is_occluded": false,
 				"is_visible": true,
-				"layer_name": child
+				"layer_name": child,
+				"source_id": child.get_cell_source_id(cell),
+				"atlas_coords": child.get_cell_atlas_coords(cell)
 				}
 	
 	if child_count == 0:
@@ -66,7 +87,7 @@ func collect_all_tiles() -> void:
 	if debug_mode:
 		print("Total TileMapLayer children: ", child_count)
 		print("total tiles: ", all_tiles_dict.size())
-		print("all tiles in the map: ", all_tiles_dict.keys())
+		print("all tiles in the map: ", all_tiles_dict)
 
 
 func collect_occluded_tiles() -> void:
@@ -97,34 +118,44 @@ func collect_occluded_tiles() -> void:
 		print(" ")
 
 
-func set_current_max_z() -> void:
+func set_level_max_z() -> void:
 	if all_tiles_dict.is_empty():
-		current_max_z = MIN_Z
+		level_max_z = MIN_Z
 		current_z = MIN_Z
 		print("Error: No tiles in the map")
 		return
 	
-	current_max_z = MIN_Z
+	level_max_z = MIN_Z
 	var has_visible_tiles = false
 	
 	for coords in all_tiles_dict.keys():
 		if all_tiles_dict[coords]["is_visible"]:
 			has_visible_tiles = true
-			if coords.z > current_max_z:
-				current_max_z = coords.z
+			if coords.z > level_max_z:
+				level_max_z = coords.z
 	
 	if not has_visible_tiles:
-		current_max_z = MIN_Z
+		level_max_z = MIN_Z
 		current_z = MIN_Z
 		print("Error: No visible tiles in the map")
 		return
 	
-	current_z = current_max_z
+	current_z = level_max_z
 	
 	# Debug Output
 	if debug_mode:
-		print("current_max_z: ", current_max_z)
+		print("levelt_max_z: ", level_max_z)
 		print(" ")
+
+
+func set_current_layer():
+	for coords in all_tiles_dict.keys():
+		if not coords.z == current_z:
+			continue
+		current_layer = all_tiles_dict[coords]["layer_name"]
+		if debug_mode:
+			print("current layer: ", current_layer)
+		return current_layer
 
 
 func apply_unselected_layer_effect() -> void:
@@ -144,19 +175,24 @@ func apply_unselected_layer_effect() -> void:
 
 func move_current_layer() -> void:
 	if Input.is_action_just_pressed("ui_up"):
-		if  current_z < current_max_z:
+		if  current_z < level_max_z:
 			current_z = current_z + ENVIRONMENT_Z_STEP
+			set_current_layer()
 			# Debug Output
 			print("current_z: ", current_z)
 	
 	if Input.is_action_just_pressed("ui_down"):
 		if  current_z > MIN_Z:
 			current_z = current_z - ENVIRONMENT_Z_STEP
+			set_current_layer()
 			# Debug Output
 			print("current_z: ", current_z)
 
 
-func get_all_tiles_on_layer(layer_z: int) -> Array[Vector2i]: # Currently not in use but might be useful
+# Helper function to get all tiles on any given layer
+# Can be called if all tiles on a layer are needed
+# Might be redundant
+func get_all_tiles_on_layer(layer_z: int) -> Array[Vector2i]: 
 	var all_tiles_on_layer: Array[Vector2i] = []
 	
 	if all_tiles_dict.is_empty():
@@ -200,11 +236,61 @@ func apply_occluded_tiles_effect() -> void:
 			)
 		
 		# Debug Output
-		if debug_mode == true:
+		if debug_mode:
 			print("replace tile: ", coords3i)
 			print("from atlas_coords: ", current_atlas_coords, ", source_id: ", current_source_id)
 			print("to atlas_coords: ", replacement_atlas_coords, ", source_id: ", replacement_source_id)
 
+
+func get_mouse_coords() -> Vector2i:
+	var mouse_pos = get_global_mouse_position()
+	var local_mouse_pos = current_layer.local_to_map(mouse_pos)
+	print(local_mouse_pos.x, ", ", local_mouse_pos.y, " on layer: ", current_layer)
+	return Vector2i(local_mouse_pos.x, local_mouse_pos.y)
+
+
+func apply_highlighted_tile_effect() -> void:
+	remove_highlighted_tile_effect()
+	var map_coords = get_mouse_coords()
+	for coords in all_tiles_dict.keys():
+		if map_coords == Vector2i(coords.x, coords.y) and all_tiles_dict[coords]["is_visible"]:
+			var current_source_id: int = current_layer.get_cell_source_id(map_coords)
+			var current_atlas_coords: Vector2i = current_layer.get_cell_atlas_coords(map_coords)
+			var current_alternative_tile = current_layer.get_cell_alternative_tile(map_coords)
+			
+			if current_source_id == all_tiles_dict[coords]["source_id"]:
+				var replacement_source_id = current_source_id + HIGHLIGHT_SOURCE_ID_STEP
+				var replacement_atlas_coords = current_atlas_coords + HIGHLIGHT_ATLAS_COORDS_STEP
+				var replacement_alternative_tile = current_alternative_tile + HIGHLIGHT_ALTERNATIVE_TILE_STEP
+			
+				current_layer.set_cell(
+					map_coords,
+					replacement_source_id,
+					replacement_atlas_coords,
+					replacement_alternative_tile
+					)
+				
+				currently_highlighted_tile = Vector3i(coords)
+			# Debug Output
+				if debug_mode:
+					print("replace tile: ", map_coords)
+					print("from atlas_coords: ", current_atlas_coords, ", source_id: ", current_source_id)
+					print("to atlas_coords: ", replacement_atlas_coords, ", source_id: ", replacement_source_id)
+
+
+func remove_highlighted_tile_effect() -> void:
+	if currently_highlighted_tile.x == -999:
+		return
+	
+	#var map_coords = get_mouse_coords()
+	if all_tiles_dict.has(currently_highlighted_tile):
+		var tile_data = all_tiles_dict[currently_highlighted_tile]
+		tile_data["layer_name"].set_cell(
+			Vector2i(currently_highlighted_tile.x, currently_highlighted_tile.y),
+			tile_data["source_id"],
+			tile_data["atlas_coords"]
+		)
+	currently_highlighted_tile = Vector3i(-999, -999, -999)
 
 
 """To do:
